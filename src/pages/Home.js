bc9f7cc17700e98f25ca3e88519414b15754e52e
lLija <ilija.trokovski@gmail.com>
2023-04-07 23:58:25 +0200

    Layouts & Nested Routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>My Home Page</div>;
+  return (
+    <div>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">The List of products</Link>
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
